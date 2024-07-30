9e466eabd41cf130963dbe74f8c781ec09e88fa8
‘abhixsh’ <aloka.abishek9@gmail.com>
2024-07-30 05:58:26 +0530

    Update build configuration and assets for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'build', // Ensure the output directory is 'build'
     rollupOptions: {
       output: {
         manualChunks(id) {
